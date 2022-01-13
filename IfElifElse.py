0d7f76ac7f97eb20afc62e088063a90019d3aacf
Mike Biacsi <mikebiacsi@shapingcloud.com>
2022-01-13 16:28:07 +0000

    Pushing new code

---
+++ IfElifElse.py
@@ -0,0 +1,8 @@
+Team = input ('input your favourite team: ')
+
+if Team == 'Southampton':
+    print('Thats my favourite team too!')
+elif Team == 'Portsmouth':
+    print('Ewww, you dirty skate bastard!')
+else:
+    print("They aren't as good as Southampton!")
